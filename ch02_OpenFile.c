eb6be220cd0b0e72679fee2455c3946547be49e2
sunabove <sunabove@nate.com>
2018-03-18 16:22:01 -0700

    aa

---
+++ ch02_OpenFile.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <stdio.h>
+
+int main( int argc, char ** argv) {
+
+    printf( "________________\n" );
+    printf( "This is a file open program.\n" );
+
+    int fd ;
+
+    fd = open( "/home/sunabove/bbb.txt" , O_RDONLY ) ; 
+
+    if( fd == -1 ) {
+        printf( "Cannot open!\n" );
+    } else {
+        printf( "Success: file open.\n" );
+    }
+
+    printf( "Good bye!\n" );
+    printf( "________________\n" );
+    
+    return 0;
+
+}
