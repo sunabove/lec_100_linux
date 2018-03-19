1931d9183d2db51daa32ea2e9fef70b262d08a35
sunabove <sunabove@nate.com>
2018-03-19 16:17:56 -0700

    __

--- ex02_wperror.c
+++ ex02_wperror.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
+#include <wchar.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
 #include <fcntl.h>
+#include <locale.h>
 
 int main(void) {
+    setlocale( LC_ALL, "" ) ;
+
     int fd = -1;
 
     // Always Reset errno before use.
     errno = 0;
 
     // Make sure you are opening a file that does not exist
-    printf( "\nTrying to open a file...." ) ; 
+    wprintf( L"\nTrying to open a file...." ) ; 
 
     fd = open("abcd",O_RDONLY);
 
     if(fd == -1) {
         // Seems like some error occured. Use strerror to print it
-        printf( "\nerrono: %d" , errno );
+        wprintf( L"\nerrono: %d" , errno );
         
-        printf("\nstrerror() says -> [%s]\n",(char*)strerror(errno));
+        wprintf( L"\nStrerror() says -> [%s]\n",(char*)strerror(errno));
         return 1;
     } else {
-        printf( "\nSuccess: open the file." );
+        wprintf( L"\nSuccess: open the file." );
     }
 
     return 0;
