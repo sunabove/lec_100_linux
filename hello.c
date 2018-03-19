1eb745ba6b34cc3a463f017318d9f614fd5f43cc
sunabove <sunabove@nate.com>
2018-03-19 16:27:20 -0700

    __

---
+++ hello.c
@@ -0,0 +1,12 @@
+/* First C program: Hello World */
+
+#include <stdio.h>
+
+int main( int argc , char ** argv )
+{
+    FILE * out = stdout ; 
+
+ 	fprintf( out, "Hello World!\n" );
+
+	return 0 ;
+}
