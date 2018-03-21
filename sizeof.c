e4788f03f35d6235629531cda60d785132e89abc
sunabove <sunaboe@nate.com>
2018-03-22 04:04:46 +0900

    __

--- sizeof.c
+++ sizeof.c
@@ -10,16 +10,20 @@ int main( int argc , char ** argv )
  	fprintf( out, "Hello World!" );
 
     char c = 'A' ; 
-
     size_t cSize = sizeof( c );  
-
     fprintf( out, "\nchar size of %c : %zu", c, cSize ); 
 
     wchar_t ga = L'가' ; 
-
     size_t gaSize = sizeof( ga );
-
     fprintf( out, "\nga char size of %c : %zu" , ga, gaSize );
+
+    char * str = "abcdefghi" ; 
+    size_t strSize = sizeof( str );
+    fprintf( out, "\nchar * size of %s : %zu" , str, strSize );
+
+    char strArr[] = "abcdefghi" ;
+    size_t strArrSize = sizeof( strArr );
+    fprintf( out, "\nchar [] size of %s : %zu" , strArr, strArrSize );
 
  	fprintf( out, "\nGood Bye!" );
 
