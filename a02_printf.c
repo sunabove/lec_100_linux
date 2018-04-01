887be7115491502ddfc60440b0b035acee3b2d2b
sunabove <sunabove@nate.com>
2018-03-31 18:21:42 -0700

    **

---
+++ a02_printf.c
@@ -0,0 +1,21 @@
+/* First C program: Hello World */
+
+#include <stdio.h>
+
+int main( int argc , char ** argv )
+{
+    FILE * out = stdout ; 
+
+ 	fprintf( out, "Hello World!" );
+	fprintf( out, "\nprintf format specifier example program.\n" );
+
+	fprintf( out, "\n\e[1mbold\e[0m" );
+	fprintf( out, "\n\e[4munderline\e[0m" );
+	fprintf( out, "\n\e[31mHello World\e[0m" ); 
+	fprintf( out, "\n\e[9mstrikethrough\e[0m" );
+	fprintf( out, "\n\e[3mitalic\e[0m" );	
+
+	fprintf( out, "\n\nGood bye!\n" );
+
+	return 0 ;
+}
