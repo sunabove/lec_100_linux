ffced242edbd5a329aa071a7f4333a4d6311bee9
sunabove <sunabove@nate.cm>
2018-05-12 08:39:25 +0900

    **

---
+++ example/a05_argument.c
@@ -0,0 +1,24 @@
+/* First C program: Hello World */
+
+#include <stdio.h>
+
+int main( int argc , char ** argv )
+{
+    FILE * out = stdout ; 
+
+	const char * LINE = "___________________________________________________\n" ; 
+    fprintf( out, "%s", LINE );
+ 	fprintf( out, "\nArgument Print Program" );
+	 fprintf( out, "\n%s", LINE );
+
+	for( int i = 0, iLen = argc ; i < iLen ; i ++ ) {
+		fprintf( out, "\nargv[%d] = %s" , i, argv[i] );
+	}
+
+	fprintf( out, "\n%s", LINE );
+    fprintf( out, "\nGood bye!" );
+    fprintf( out, "\n%s", LINE ); 
+    fflush( out );
+
+	return 0 ;
+}
