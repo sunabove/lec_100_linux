a788461b8ce77aa7339715c1cb2423add6d2372a
sunabove <sunabove@nate.com>
2018-05-27 00:41:45 +0900

    **

--- net_01/client.c
+++ net_01/client.c
@@ -7,38 +7,40 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
-void error(char *msg) {
-    perror(msg); 
-}
-
 int main(int argc, char *argv[]) {
-    FILE * console = stdout ;
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
+    FILE * console = stdout ; 
+    
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        error("ERROR opening socket"); 
-    }
-    server = gethostbyname(argv[1]);
+    
+    struct hostent *server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
+    
+    int portno = atoi(argv[2]);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("ERROR opening socket"); 
+        exit( 1 );
+    }
+
+    struct sockaddr_in serv_addr;
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0) {
-        error("ERROR connecting");
+    
+    if (connect(sockfd, &serv_addr,sizeof(serv_addr)) < 0) {
+        perror("ERROR connecting");
+        exit( 1 );
     }
+
     int valid = 1 ;
+    int n ; 
     char buff[256];    
     while( valid ) {
         fprintf( console, "Please enter the message: ");
@@ -52,13 +54,13 @@ int main(int argc, char *argv[]) {
             n = write(sockfd,buff,strlen(buff));
             if ( 0 > n ) {
                 valid = 0 ;
-                error("ERROR writing to socket");
+                perror("ERROR writing to socket");
             } else if ( 0 < n ) {
                 bzero(buff,256);
                 n = read(sockfd,buff,255);
                 if ( 0 > n ) {
                     valid = 0 ; 
-                    error("ERROR reading from socket");
+                    perror("ERROR reading from socket");
                 } else if( 0 < n ) {
                     fprintf( console, "%s\n",buff);
                     fflush( console );
@@ -66,5 +68,8 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+
+    fprintf( console, "\nGood bye!\n" );
+
     return 0;
 }
