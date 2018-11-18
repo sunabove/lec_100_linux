5f3e39fd5374820394beb7aff22bba9888478428
sunabove <sunabove@nate.com>
2018-11-18 21:43:46 +0900

    **

--- p_03_file_io/shape_main/Shape.cpp
+++ p_03_file_io/shape_main/Shape.cpp
@@ -44,147 +44,148 @@ namespace esri {
         return rn ; 
     }
 
-    int readShapeFile( ShapeFile * shapeFile, FILE * file ) {
-	
+    // readShapeFile
+    int readShapeFile( ShapeFile * shapeFile, FILE * file ) {	
         const char * LINE = "________________________________" ; 
 
-        if( 0 == errno ) {
-            size_t nr = 1 ;
+        size_t nr = 1 ;
 
-            MainFileHeader * p = & shapeFile->mainFileHeader ;
+        if( 0 != errno ) {
+            nr = 0 ; 
+        }
 
-            nr = nr and readInteger( file, & p->fileCode, true );
-            
-            nr = nr and readInteger( file, & p->unused_1 );
-            nr = nr and readInteger( file, & p->unused_2 );
-            nr = nr and readInteger( file, & p->unused_3 );
-            nr = nr and readInteger( file, & p->unused_4 );
-            nr = nr and readInteger( file, & p->unused_5 );
-            
-            nr = nr and readInteger( file, & p->fileLength, true );
-            nr = nr and readInteger( file, & p->version );
-            nr = nr and readInteger( file, & p->shapeType );
+        MainFileHeader * p = & shapeFile->mainFileHeader ;
 
-            nr = nr and readDouble( file, & p->box.x.min );
-            nr = nr and readDouble( file, & p->box.y.min );
-            nr = nr and readDouble( file, & p->box.x.max );
-            nr = nr and readDouble( file, & p->box.y.max );
-            nr = nr and readDouble( file, & p->box.z.min );
-            nr = nr and readDouble( file, & p->box.z.max );
-            nr = nr and readDouble( file, & p->box.m.min );
-            nr = nr and readDouble( file, & p->box.m.max );
-            
+        nr = nr and readInteger( file, & p->fileCode, true );
+        
+        nr = nr and readInteger( file, & p->unused_1 );
+        nr = nr and readInteger( file, & p->unused_2 );
+        nr = nr and readInteger( file, & p->unused_3 );
+        nr = nr and readInteger( file, & p->unused_4 );
+        nr = nr and readInteger( file, & p->unused_5 );
+        
+        nr = nr and readInteger( file, & p->fileLength, true );
+        nr = nr and readInteger( file, & p->version );
+        nr = nr and readInteger( file, & p->shapeType );
+
+        nr = nr and readDouble( file, & p->box.x.min );
+        nr = nr and readDouble( file, & p->box.y.min );
+        nr = nr and readDouble( file, & p->box.x.max );
+        nr = nr and readDouble( file, & p->box.y.max );
+        nr = nr and readDouble( file, & p->box.z.min );
+        nr = nr and readDouble( file, & p->box.z.max );
+        nr = nr and readDouble( file, & p->box.m.min );
+        nr = nr and readDouble( file, & p->box.m.max );
+        
+        if( 0 == errno and nr ) {
+            p->fileLength = 2 * p->fileLength ;
+
+            ZF_LOGI( "%s", LINE );
+            ZF_LOGI( "MainFileHeader" );
+            ZF_LOGI( "File Code        = %u" , p->fileCode );
+            ZF_LOGI( "file Length      = %u" , p->fileLength );
+            ZF_LOGI( "Version          = %u" , p->version );
+            ZF_LOGI( "Shape Type       = %u" , p->shapeType );
+            ZF_LOGI( "Bouding Box Xmin = %+011.6f" , p->box.x.min ); 
+            ZF_LOGI( "Bouding Box Ymin = %+011.6f" , p->box.y.min ); 
+            ZF_LOGI( "Bouding Box Xmax = %+011.6f" , p->box.x.max ); 
+            ZF_LOGI( "Bouding Box Ymax = %+011.6f" , p->box.y.max ); 
+            ZF_LOGI( "Bouding Box Zmin = %+011.6f" , p->box.z.min ); 
+            ZF_LOGI( "Bouding Box Zmax = %+011.6f" , p->box.z.max ); 
+            ZF_LOGI( "Bouding Box Mmin = %+011.6f" , p->box.m.min ); 
+            ZF_LOGI( "Bouding Box Mmax = %+011.6f" , p->box.m.max );
+            ZF_LOGI( "%s", LINE );
+        }
+
+        const auto shapeType = p->shapeType ; 
+
+        auto shapes = & shapeFile->shapes ; 
+
+        while( 0 == errno and nr ) { 			
+            ZF_LOGI( "%s", LINE );
+
+            Header header ; 
+            nr = nr and readInteger( file, & header.recordNo, true );
+            nr = nr and readInteger( file, & header.contentLength, true );
+
             if( 0 == errno and nr ) {
-                p->fileLength = 2 * p->fileLength ;
-
-                ZF_LOGI( "%s", LINE );
-                ZF_LOGI( "MainFileHeader" );
-                ZF_LOGI( "File Code        = %u" , p->fileCode );
-                ZF_LOGI( "file Length      = %u" , p->fileLength );
-                ZF_LOGI( "Version          = %u" , p->version );
-                ZF_LOGI( "Shape Type       = %u" , p->shapeType );
-                ZF_LOGI( "Bouding Box Xmin = %+011.6f" , p->box.x.min ); 
-                ZF_LOGI( "Bouding Box Ymin = %+011.6f" , p->box.y.min ); 
-                ZF_LOGI( "Bouding Box Xmax = %+011.6f" , p->box.x.max ); 
-                ZF_LOGI( "Bouding Box Ymax = %+011.6f" , p->box.y.max ); 
-                ZF_LOGI( "Bouding Box Zmin = %+011.6f" , p->box.z.min ); 
-                ZF_LOGI( "Bouding Box Zmax = %+011.6f" , p->box.z.max ); 
-                ZF_LOGI( "Bouding Box Mmin = %+011.6f" , p->box.m.min ); 
-                ZF_LOGI( "Bouding Box Mmax = %+011.6f" , p->box.m.max );
-                ZF_LOGI( "%s", LINE );
-
+                ZF_LOGI( "Record Number  = %d", header.recordNo );
+                ZF_LOGI( "Content Length = %d", header.contentLength );
             }
 
-            const auto shapeType = p->shapeType ; 
+            Shape * shape = NULL ; 
+        
+            if( 0 != errno or 1 > nr ) {
+                // do nothing.
+            } else if( 1 == shapeType ) { // Point
+                auto point = new Point() ;
+                //Point * point = new Point() ;
+                shape = point ; 
+                shape->shapeType = shapeType ;
 
-            auto shapes = & shapeFile->shapes ; 
+                nr = nr and readInteger( file, & shape->shapeType );
 
-            while( 0 == errno and nr ) { 			
-                ZF_LOGI( "%s", LINE );
-
-                Header header ; 
-                nr = nr and readInteger( file, & header.recordNo, true );
-                nr = nr and readInteger( file, & header.contentLength, true );
+                nr = nr and readDouble( file, & point->x );
+                nr = nr and readDouble( file, & point->y );
 
                 if( 0 == errno and nr ) {
-                    ZF_LOGI( "Record Number  = %d", header.recordNo );
-                    ZF_LOGI( "Content Length = %d", header.contentLength );
+                    ZF_LOGI( "Shape Type  = %d", shape->shapeType);
+                    ZF_LOGI( "X = %+011.6f", point->x );
+                    ZF_LOGI( "Y = %+011.6f", point->y ); 
+                }
+            } else if( 3 == shapeType || 5 == shapeType ) { // Polyline, Polygon
+                Polyline * poly = NULL ; 
+                if ( 3 == shapeType ) { 
+                    poly = new Polyline() ; 
+                } else if ( 5 == shapeType ) { 
+                    poly = new Polygon() ;
                 }
 
-                Shape * shape = NULL ; 
-            
-                if( 0 != errno or 1 < nr ) {
-                    // do nothing.
-                } else if( 1 == shapeType ) { // Point
-                    auto point = new Point() ;
-                    //Point * point = new Point() ;
-                    shape = point ; 
-                    shape->shapeType = shapeType ;
+                shape = poly; 
+                shape->shapeType = shapeType ;
 
-                    nr = nr and readInteger( file, & shape->shapeType );
+                nr = nr and readInteger( file, & shape->shapeType );
 
-                    nr = nr and readDouble( file, & point->x );
-                    nr = nr and readDouble( file, & point->y );
+                auto box = & poly->box;
 
-                    if( 0 == errno and nr ) {
-                        ZF_LOGI( "Shape Type  = %d", shape->shapeType);
-                        ZF_LOGI( "X = %+011.6f", point->x );
-                        ZF_LOGI( "Y = %+011.6f", point->y ); 
-                    }
-                } else if( 3 == shapeType || 5 == shapeType ) { // Polyline, Polygon
-                    Polyline * poly = NULL ; 
-                    if ( 3 == shapeType ) { 
-                        poly = new Polyline() ; 
-                    } else if ( 5 == shapeType ) { 
-                        poly = new Polygon() ;
-                    }
+                nr = nr and readDouble( file, & box->x.min );
+                nr = nr and readDouble( file, & box->y.min );
+                nr = nr and readDouble( file, & box->x.max );
+                nr = nr and readDouble( file, & box->y.max );
 
-                    shape = poly; 
-                    shape->shapeType = shapeType ;
+                nr = nr and readInteger( file, & poly->numParts );
+                nr = nr and readInteger( file, & poly->numPoints );
 
-                    nr = nr and readInteger( file, & shape->shapeType );
+                auto numParts = poly->numParts; 
 
-                    auto box = & poly->box;
+                int32_t * parts = new int[numParts] ; 
+                poly->parts = parts ;
 
-                    nr = nr and readDouble( file, & box->x.min );
-                    nr = nr and readDouble( file, & box->y.min );
-                    nr = nr and readDouble( file, & box->x.max );
-                    nr = nr and readDouble( file, & box->y.max );
-
-                    nr = nr and readInteger( file, & poly->numParts );
-                    nr = nr and readInteger( file, & poly->numPoints );
-
-                    auto numParts = poly->numParts; 
-
-                    int32_t * parts = new int[numParts] ; 
-                    poly->parts = parts ;
-
-                    for( int i = 0, iLen = numParts ; nr && i < iLen ; i ++ ) {
-                        nr = nr and readInteger( file, parts ); 
-                        parts ++ ;
-                    }
-
-                    auto numPoints = poly->numPoints ; 
-                    Point * points = new Point[ numPoints ] ; 
-                    poly->points = points ; 
-
-                    for( int i = 0, iLen = numPoints ; nr && i < iLen ; i ++ ) {
-                        nr = nr and readDouble( file, & points->x );
-                        nr = nr and readDouble( file, & points->y );
-                        points ++ ; 
-                    }
-
-                    if( 0 == errno and nr ) {
-                        ZF_LOGI( "Shape Type  = %d", shape->shapeType);
-                        ZF_LOGI( "numParts    = %d", poly->numParts );
-                        ZF_LOGI( "numPoints   = %d", poly->numParts ); 
-                    }
+                for( int i = 0, iLen = numParts ; nr && i < iLen ; i ++ ) {
+                    nr = nr and readInteger( file, parts ); 
+                    parts ++ ;
                 }
 
-                if( NULL != shape ) {
-                    shape->header = header ; 
-                    shapes->push_back( * shape ); 
+                auto numPoints = poly->numPoints ; 
+                Point * points = new Point[ numPoints ] ; 
+                poly->points = points ; 
+
+                for( int i = 0, iLen = numPoints ; nr && i < iLen ; i ++ ) {
+                    nr = nr and readDouble( file, & points->x );
+                    nr = nr and readDouble( file, & points->y );
+                    points ++ ; 
                 }
+
+                if( 0 == errno and nr ) {
+                    ZF_LOGI( "Shape Type  = %d", shape->shapeType);
+                    ZF_LOGI( "numParts    = %d", poly->numParts );
+                    ZF_LOGI( "numPoints   = %d", poly->numParts ); 
+                }
+            }
+
+            if( NULL != shape ) {
+                shape->header = header ; 
+                shapes->push_back( * shape ); 
             }
         }
 
@@ -198,5 +199,6 @@ namespace esri {
 
         return 0 == errno ;
     }
+    // -- readShapeFile
     
 }
