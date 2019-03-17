a28d580c8a472497847b6c953f03903aa0c68801
sunabove <sunabove@users.noreply.github.com>
2019-03-17 11:30:50 +0900

    **

--- example/a02_printf_02.c
+++ example/a02_printf_02.c
@@ -7,10 +7,10 @@ int main( int argc , char ** argv ) {
 
  	fprintf( out, "Program : printf example\n" ); 
 
-	fprintf( out, "%d\n",0) ;
-	fprintf( out, "%d\n",-7) ;
-	fprintf( out, "%d\n",1560133635) ;
-	fprintf( out, "%d\n",-2035065302) ;  
+	fprintf( out, "%d\n", 0);
+	fprintf( out, "%d\n", -7);
+	fprintf( out, "%d\n", 1560133635);
+	fprintf( out, "%d\n", -2035065302);
 
 	fprintf( out, "Good bye!\n" );
 
