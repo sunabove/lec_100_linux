19c706db4536743a10ecd1f5bb5af34e6fbb3919
sunabove <sunabove@nate.com>
2019-03-18 22:35:59 +0900

    **

--- example/a02_printf_02.c
+++ example/a02_printf_02.c
@@ -8,42 +8,42 @@ int main( int argc , char ** argv ) {
  	fprintf( out, "Program : printf example\n" ); 
 
 	// integer
-	fprintf( out, "Integer example\n", 0);
+	fprintf( out, "Integer example\n" );
 	fprintf( out, "%d\n", 0);
 	fprintf( out, "%d\n", -7);
 	fprintf( out, "%d\n", 1560133635);
 	fprintf( out, "%d\n\n", -2035065302);
 
 	// width option
-	fprintf( out, "Width option\n", 0);
+	fprintf( out, "Width option\n" );
 	fprintf( out, "%5d\n",0);
 	fprintf( out, "%5d\n",-7);
 	fprintf( out, "%5d\n",1560133635) ;
 	fprintf( out, "%5d\n\n",-2035065302) ;
 
 	// justify option
-	fprintf( out, "Width option\n", 0);
+	fprintf( out, "Width option\n" );
 	fprintf( out, "%-5d\n",0) ;
 	fprintf( out, "%-5d\n",-7) ;
 	fprintf( out, "%-5d\n",1560133635) ;
 	fprintf( out, "%-5d\n\n",-2035065302) ;
 
 	// zero-fill option
-	fprintf( out, "Zero fill option\n", 0);
+	fprintf( out, "Zero fill option\n" );
 	fprintf( out, "%05d\n",0) ;
 	fprintf( out, "%05d\n",-7) ;
 	fprintf( out, "%05d\n",1560133635) ;
 	fprintf( out, "%05d\n\n",-2035065302);
 
 	// plus signs option
-	fprintf( out, "Plus signs\n", 0);
+	fprintf( out, "Plus signs\n" );
 	fprintf( out, "%+5d\n",0) ;
 	fprintf( out, "%+5d\n",-7) ;
 	fprintf( out, "%+5d\n",1560133635) ;
 	fprintf( out, "%+5d\n\n",-2035065302) ;
 
 	// invisible plus sign
-	fprintf( out, "Invisible plus signs\n", 0);
+	fprintf( out, "Invisible plus signs\n" );
 	fprintf( out, "%+-5d\n",0) ;
 	fprintf( out, "%+-5d\n",-7) ;
 	fprintf( out, "%+-5d\n",1560133635) ;
@@ -58,7 +58,7 @@ int main( int argc , char ** argv ) {
 	fprintf( out, "%-5d\n\n",-2035065302) ;
 
 	// plus, space and zero
-	fprintf( out, "Plus, space and zero\n", 0);
+	fprintf( out, "Plus, space and zero\n" );
 	fprintf( out, "% 05d\n",0)          ;
 	fprintf( out, "% 05d\n",-7)         ;
 	fprintf( out, "% 05d\n",1560133635) ;
@@ -69,7 +69,7 @@ int main( int argc , char ** argv ) {
 	fprintf( out, "%+05d\n\n",-2035065302);
 
 	// Strings
-	fprintf( out, "Strings\n", 0);
+	fprintf( out, "Strings\n" );
 	fprintf( out, "%5s\n","") ;
 	fprintf( out, "%5s\n","a") ;
 	fprintf( out, "%5s\n","ab") ;
@@ -81,7 +81,7 @@ int main( int argc , char ** argv ) {
 
 	// Floating point
 	float e = 2.718281828 ; 
-	fprintf( out, "Floating point\n", 0);
+	fprintf( out, "Floating point\n" );
 	fprintf( out, "%.0f\n",e)   ;
 	fprintf( out, "%.0f.\n",e)  ;
 	fprintf( out, "%.1f\n",e)   ;
